6bf1589c280f5df080414c7669b19b9908a473b4
Josie Ku <josie.ku@hotmail.com>
2020-04-29 09:15:54 +0800

    updated links for different prod environments

--- src/Constants/links.js
+++ src/Constants/links.js
@@ -1,1 +1,1 @@
-export const SERVER_URL = process.env.NODE_ENV === "production" ? "http://0.0.0.0:5000/" : "https://instapic-backend.herokuapp.com/";
+export const SERVER_URL = process.env.NODE_ENV === "development" ? "http://0.0.0.0:5000/" : "https://instapic-backend.herokuapp.com/";
